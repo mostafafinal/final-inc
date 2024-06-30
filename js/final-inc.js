c54aa6e645383feba9de6b5b40b50bbff5821a4d
Mostafa <mostafafinal55@gmail.com>
2024-07-01 01:47:06 +0300

    Adding ScrollToTop button feature

--- js/final-inc.js
+++ js/final-inc.js
@@ -1,6 +1,6 @@
 // Loading
 let loading = document.querySelector(".loading");
-this.onload = function () {
+window.onload = function () {
   setTimeout(() => {
     loading.classList.add("hidden");
     document.body.style.overflow = "visible";
@@ -8,4 +8,18 @@ this.onload = function () {
       loading.remove();
     }, 3000);
   }, 3000);
+};
+
+// Scroll Up Bottom
+let scrollBtn = document.querySelector(".scroll-up");
+window.onscroll = function () {
+  scrollY >= 700
+    ? scrollBtn.classList.remove("hidden")
+    : scrollBtn.classList.add("hidden");
+};
+scrollBtn.onclick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 };
